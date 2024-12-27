021eff4ed5357cb76887744c1336151240111593
Bruno <brunosilvaf22@hotmail.com>
2024-12-27 00:10:54 -0300

    add/down balance

--- src/routes/paymentsRoutes.ts
+++ src/routes/paymentsRoutes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import {
+  addBalanceController,
   createPixPayment,
+  subtractBalanceController,
   webhookHandler,
 } from '../controllers/paymentsController';
 
@@ -9,5 +11,7 @@ const router = Router();
 // Rota para gerar um pagamento
 router.post('/payments', createPixPayment);
 router.post('/payments/webhook', webhookHandler);
+router.post('/payments/subtract-balance', subtractBalanceController);
+router.post('/payments/add-balance', addBalanceController);
 
 export default router;
